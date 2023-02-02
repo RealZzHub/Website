dca285d07bfe4e595cb8470c29173063931e2926
DecerZz <59297516+DecerZz@users.noreply.github.com>
2023-02-02 02:39:19 +0200

    Update app.js

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ particlesJS('particles-js',
         }
       },
       "color": {
-        "value": "#a64fdb"
+        "value": "#79aaf9"
       },
       "shape": {
         "type": "circle",
@@ -63,7 +63,7 @@ particlesJS('particles-js',
       "line_linked": {
         "enable": true,
         "distance": 150,
-        "color": "#a64fdb",
+        "color": "#79aaf9",
         "opacity": 0.4,
         "width": 1
       },
